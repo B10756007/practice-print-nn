85a40f4f66c8dafafb1640197f9aac031177c325
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:14:48 +0800

    加入專案檔案。

---
+++ practice print nn/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace practice_print_nn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入N:");
+
+            int x;
+            x = int.Parse(Console.ReadLine());
+
+            for(int i = 1 ; i < x + 1; i++)
+            {
+                Console.WriteLine(x +  " * " + i  );
+            }
+        }
+    }
+}
